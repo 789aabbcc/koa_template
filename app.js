4e57ab4e9de8da37157a80a36f34b4bed294cd9e
好一只帅卤蛋 <37758284+789aabbcc@users.noreply.github.com>
2020-04-09 15:04:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,62 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+// // Token拦截中间件,必须写在路由前面
+// // 不拦截接口
+// let excludeURL = [
+//   "/login",
+//   "/register"
+// ]
+// function inArray(search, Array) {
+//   for (let i in Array) {
+
+//     if (search.startsWith(Array[i])) {
+//       return true;
+//     }
+//     return false;
+//   }
+// }
+
+// app.use(async (ctx, next) => {
+//   let url = ctx.request.url;
+//   if (inArray(url, excludeURL)) {
+//     await next();
+//     return;
+//   }
+
+//   let token;
+
+//   //接收token
+//   if (ctx.request.query.token) {
+//     token = ctx.request.query.token;
+//   } else if (ctx.request.header.token) {
+//     token = ctx.request.header.token;
+//   } else {
+//     ctx.response.body = {
+//       msg: "未登录禁止访问"
+//     }
+//     return;
+//   }
+
+//   let data = Token.decrypt(token);
+
+
+//   if (data.token) {
+
+//     await next();
+
+//   } else {
+//     //无效token
+//     ctx.response.body = {
+//       msg: "无效token"
+//     }
+//     return;
+//   }
+
+// })
+
+
+
 // routes
 app.use(route);
 
@@ -36,6 +92,6 @@ app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
 
-app.listen(3004, () => {
+app.listen(3000, () => {
   console.log("服务器3000端口启动成功!");
 })
